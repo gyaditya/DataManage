c64603cc8ae8a66de8a7631cf00af621c83aa7e4
gyaditya <112659420+gyaditya@users.noreply.github.com>
2022-12-02 14:42:04 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,51 @@
-print("Hello world")
+#Data Management by Adi
+
+#Inizialize the list
+
+#Set Loop True
+ProgramLoop = True
+
+#Start Looping
+while ProgramLoop:
+
+  #Options To pick From
+    print("OPTION 1: Display all of the data")
+    print("OPTION 2: Display some of the data")
+    print("OPTION 3: Sort the data")
+    print("OPTION 4: Select data to add to a favourites list / shopping cart")
+    print("OPTION 5: Display favourites list / shopping cart")
+    print("OPTION 6: Exit")
+
+    #Input From The user
+    userInput = input("Pick your Option:")
+
+    #Option 1 - Display Contact Names
+    if(userInput == "1"):
+        print("PlaceHolder")
+    
+
+    #Option 2 - Search for Contact
+    elif(userInput == "2"):
+        print("PlaceHolder")
+
+
+    #Option 3 - Edit Contact
+    elif(userInput == "3"):
+        print("PlaceHolder")
+       
+
+    #Option 4 - New Contact
+    elif (userInput == "4"):
+        print("PlaceHolder")
+
+
+    #Option 5 - Remove Contact
+    elif(userInput == "5"):
+        print("PlaceHolder")
+
+
+    #Option 6 - End Loop
+    elif(userInput == "6"):
+        Programloop = False
+        break
+    
